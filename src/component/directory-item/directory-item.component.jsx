a2e8dbbff468f599616b2bb8e71de3f64eb7e4af
priyankachetty <priyankachetty.m@outlook.com>
2023-02-06 23:10:49 -0500

    added link from home page to different categories

--- src/component/directory-item/directory-item.component.jsx
+++ src/component/directory-item/directory-item.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import './directory-item.style.scss';
 
 const DirectoryItem = ({category}) =>{
@@ -11,7 +13,7 @@ const DirectoryItem = ({category}) =>{
 						}}
 					/>
 					<div className="body">
-						<h2>{title}</h2>
+						<Link to={`/shop/${title}`}><h2>{title}</h2></Link>
 						<p>Shop Now</p>
 					</div>
 				</div>
